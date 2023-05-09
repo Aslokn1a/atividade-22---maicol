32f9eac451686f3ee978436e89e7d23bc8e0d93d
Aslokn1a <Thiago_baierski@estudante.sesisenai.org.br>
2023-05-09 09:39:58 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,15 @@
+document.onkeydown = Pega_Tecla;
+
+function Pega_Tecla(e) 
+{	
+    e = e || window.event;
+
+    tecla = e.keyCode;
+    console.log(tecla); // MOSTRA O CÓDIGO DA TECLA PRESSIONADA
+
+}
+
+function colorir()
+{
+    
+}
